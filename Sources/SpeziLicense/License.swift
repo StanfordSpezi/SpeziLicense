894c208556e75329eeac5e553cfcadbcac5d3908
NikolaiMadlener <nikolai@madlener.io>
2024-04-03 21:23:52 +0200

    add documentation to all public declarations

--- Sources/SpeziLicense/License.swift
+++ Sources/SpeziLicense/License.swift
@@ -11,7 +11,9 @@ import Foundation
 
 /// License struct to be passed to `ContributionsList`
 public struct License {
+    /// The full name of the License
     public let name: String
+    /// The standardized SPDX-License-Identifier
     public let spdxIdentifier: String
     
     /// - Parameters:
@@ -26,19 +28,27 @@ public struct License {
 
 /// Extensions for some of the most popular licenses
 extension License {
+    /// Represents the MIT License
     public static let mit = License(name: "MIT License", spdxIdentifier: "MIT")
     
+    /// Represents the Apache License 2.0
     public static let apachev2 = License(name: "Apache License 2.0", spdxIdentifier: "Apache-2.0")
     
+    /// Represents the GNU General Public License
     public static let gplv2only = License(name: "GNU General Public License", spdxIdentifier: "GPL-2.0-only")
     
+    /// Represents the GNU General Public License version 2.0
     public static let gplv2later = License(name: "GNU General Public License version 2.0", spdxIdentifier: "GPL-2.0-or-later")
     
+    /// Represents the BSD 2-Clause "Simplified" License
     public static let bsd2 = License(name: "BSD 2-Clause \"Simplified\" License", spdxIdentifier: "BSD-2-Clause")
     
+    /// Represents the BSD 3-Clause "New" or "Revised" License
     public static let bsd3 = License(name: "BSD 3-Clause \"New\" or \"Revised\" License", spdxIdentifier: "BSD-3-Clause")
     
+    /// Represents the BSD 4-Clause "Original" or "Old" License
     public static let bsd4 = License(name: "BSD 4-Clause \"Original\" or \"Old\" License", spdxIdentifier: "BSD-4-Clause")
     
+    /// Represents the zlib License
     public static let zlib = License(name: "zlib License", spdxIdentifier: "Zlib")
 }
