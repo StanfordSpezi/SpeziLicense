3eb1b75772d5e37f1826bd372ffaa586428cac7f
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-04-03 16:28:42 -0700

    Fix SwiftLint

--- Sources/SpeziLicense/Package+LicenseType.swift
+++ Sources/SpeziLicense/Package+LicenseType.swift
@@ -13,24 +13,24 @@ import SwiftPackageList
 extension License {
     // Constants representing typical text and regular expression patterns often found in license files.
     // They are used for matching and identifying different types of licenses within text documents.
-    static private let mitText = "MIT License"
-    static private let apacheText = "Apache License"
-    static private let gnuText = "GNU GENERAL PUBLIC LICENSE"
-    static private let bsdTwoClauseText =
+    private static let mitText = "MIT License"
+    private static let apacheText = "Apache License"
+    private static let gnuText = "GNU GENERAL PUBLIC LICENSE"
+    private static let bsdTwoClauseText =
         """
         Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met
         """
-    static private let bsdThreeClausePattern =
+    private static let bsdThreeClausePattern =
         """
         Neither the name of (.+) nor the names of (.+) may be used to endorse or promote products derived from this software \
         without specific prior written permission
         """
-    static private let bsdFourClauseText =
+    private static let bsdFourClauseText =
         """
         All advertising materials mentioning features or use of this software must display the following acknowledgement: \
         this product includes software developed by
         """
-    static private let zlibPattern =
+    private static let zlibPattern =
         """
         The origin of this software must not be misrepresented; you must not claim that you wrote the original software. \
         If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.(.*) \
